3b64975ee3f01cf16c790173a3a21a4d326b514f
pwsgoooo <pws_01@naver.com>
2024-01-25 18:08:56 +0900

    240125

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ejs = require("ejs");
 const app = express();
 const port = 3000;
 var bodyParser = require("body-parser");
+const mysql = require("mysql2");
 
 app.set("view engine", "ejs");
 // app.set("views", path.join(__dirname, "views"));
@@ -10,13 +11,17 @@ app.set("views", "./views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const connecection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "0000",
+  database: "make_web",
+});
+
 app.get("/", (req, res) => {
   res.render("home"); // ./views/index.ejs
 });
-
-// app.get("/", (req, res) => {
-//   res.render("index"); // ./views/index.ejs
-// });
 
 app.get("/profile", (req, res) => {
   res.render("profile");
@@ -36,16 +41,16 @@ app.post("/contactProc", (req, res) => {
   const email = req.body.email;
   const memo = req.body.memo;
 
-  // var sql =
-  //   'inset into contact(name,phons,email,memo,regdate) values("${name}","${phone}","${email}","${memo}", now())';
+  var sql =
+    'insert into make_web.contact(name,phone,email,memo,regdate) values("${name}","${phone}","${email}","${memo}", now())';
 
-  // connecection.query(sql, function (err, result) {
-  //   if (err) throw err;
-  //   console.log("자료 1개가 삽입되었습니다.");
-  //   res.send(
-  //     "<script> alert(문의사항이 등록되었습니다.); locatio.href='/' </script>"
-  //   );
-  // });
+  connecection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("자료 1개가 삽입되었습니다.");
+    res.send(
+      "<script> alert(문의사항이 등록되었습니다.); locatio.href='/' </script>"
+    );
+  });
 
   var a = "{$name} ${phone} ${email} ${memo}";
   res.send(a);
